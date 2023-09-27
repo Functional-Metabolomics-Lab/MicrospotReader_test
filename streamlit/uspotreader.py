79e4886637d527a2a7ffca89ba3fca47cdb66f87
Simon Knoblauch <simonb.knoblauch+git@gmail.com>
2023-09-27 17:58:26 +0200

    added streamlit file

---
+++ streamlit/uspotreader.py
@@ -0,0 +1,10 @@
+# Importing dependencies
+import streamlit as st
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+from pathlib import Path
+import math
+import microspot_util as msu
+
+st.title("Microspot Reader")
